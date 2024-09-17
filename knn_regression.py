2aa025a7bf9efa9d04b2087b8d7e406699142015
Cybersasa <josephngunjiri2024@gmail.com>
2024-09-17 21:28:30 +0300

    display of how knn model works

--- knn_regression.py
+++ knn_regression.py
@@ -19,12 +19,24 @@ from sklearn.neighbors import KNeighborsClassifier
 #knn for regression
 from sklearn.neighbors import KNeighborsRegressor
 
-X, y = mglearn.datasets.make_wave(n_samples=40)
+#X, y = mglearn.datasets.make_wave(n_samples=40)
 
+'''
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
 reg = KNeighborsRegressor(n_neighbors=3)
 reg.fit(X, y)
 
-#making predic
+#making predictions from the new regression model
+#to be able to assess accuracy of the predictions, we make predictions
+#on the test set of our data
+#print('Predicted Value are : {}'.format(reg.predict(X_test)))
 
+#comparing predicted values vs actual values
+pred = reg.predict(X_test)
+for i in range(1, len(pred+1)):
+    pred
+    print('{} vs {}'.format(pred[i], y_test[i]))
+'''
+
+mglearn.plots.plot_knn_regression(n_neighbors=1)
