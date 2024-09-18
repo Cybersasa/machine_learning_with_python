7375931809abf7c96f50fdda2ae365e1fc2ee360
Cybersasa <josephngunjiri2024@gmail.com>
2024-09-19 02:25:31 +0300

    observe how knn regression works, launch linear regression model
    observe how knn regression works, how its accuracy is measured (using R^2) and how to find optimal random state, and optimal number of neighbors.
    Realized there is no way to optimize both values to get an optimal result

--- knn_regression.py
+++ knn_regression.py
@@ -17,26 +17,51 @@ from sklearn.neighbors import KNeighborsClassifier
 
 
 #knn for regression
-from sklearn.neighbors import KNeighborsRegressor
+#from sklearn.neighbors import KNeighborsRegressor
 
-#X, y = mglearn.datasets.make_wave(n_samples=40)
+X, y = mglearn.datasets.make_wave(n_samples=40)
 
-'''
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-reg = KNeighborsRegressor(n_neighbors=3)
-reg.fit(X, y)
+#reg = KNeighborsRegressor(n_neighbors=3)
+#reg.fit(X_train, y_train)
 
 #making predictions from the new regression model
 #to be able to assess accuracy of the predictions, we make predictions
 #on the test set of our data
 #print('Predicted Value are : {}'.format(reg.predict(X_test)))
 
-#comparing predicted values vs actual values
-pred = reg.predict(X_test)
-for i in range(1, len(pred+1)):
-    pred
-    print('{} vs {}'.format(pred[i], y_test[i]))
-'''
+#Evaluating performace of model using R^2
+#print('The model accuracy is {:.2f}'.format(100*(reg.score(X_test, y_test))))
 
-mglearn.plots.plot_knn_regression(n_neighbors=1)
+#visualizing regression through knn model
+#mglearn.plots.plot_knn_regression(n_neighbors=1)
+
+##testing optimal neighbours, then optimal random_state
+
+#finding optimal number of neighbours (highest R-squared)
+X, y = mglearn.datasets.make_wave(n_samples=40)
+#print('Labels in Cancer dataset are: {}'.format(cancer.keys()))
+#from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=72277)
+from sklearn.neighbors import KNeighborsRegressor
+scores = []
+for i in range(1,31):
+    model = KNeighborsRegressor(n_neighbors=i)
+    model.fit(X_train, y_train)
+    scores.append(model.score(X_test, y_test))
+print('Highest accuracy is {:.2f} occuring {} times at position {} so best n_neighbors={}'.format(max(scores), scores.count(max(scores)), scores.index(max(scores)), scores.index(max(scores))+1))
+#optimal n_neighbours determined to be 3, yielding a model accuracy of 83%
+
+
+#with n_neighbors = 3, find optimal random_state value
+scores = []
+for i in range(0, 1000001):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=i)
+    model = KNeighborsRegressor(n_neighbors=3)
+    model.fit(X_train, y_train)
+    scores.append(model.score(X_test, y_test))
+print('Highest accuracy is {:.2f} occuring {} times at position {} so best random_state={}'.format(max(scores), scores.count(max(scores)), scores.index(max(scores)), scores.index(max(scores))+1))
+  
+#for 3 neighbors, optimal random_state=72277 with an ccuracy of 0.97
+
---
+++ linear_regression.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 18 15:43:28 2024
+
+@author: Cybersasa
+"""
+import mglearn 
+# first observe the plot and then learn how to generate it
+mglearn.plots.plot_linear_regression_wave()
