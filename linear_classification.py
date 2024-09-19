2110478b6a96b9ae8f660f95a790cfd325e813bb
Cybersasa <josephngunjiri2024@gmail.com>
2024-09-19 20:25:46 +0300

    Update linear_classification.py

--- linear_classification.py
+++ linear_classification.py
@@ -14,3 +14,13 @@ from matplotlib import pyplot as plt
 
 X, y = mglearn.datasets.make_forge()
 fig, axes = plt.subplots(1, 2, figsize=(10,3))
+
+for model, ax in zip([LinearSVC(), LogisticRegression()], axes):
+    clf = model.fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=False, eps=0.5,\
+                                    ax=ax, alpha=0.7)
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title("{}".format(clf.__class__.__name__))
+    ax.set_xlabel('Feature 0')
+    ax.set_ylabel('Feature 1')
+axes[0].legend()
