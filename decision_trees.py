2216d608e70ef0aff18e8ebd8051ee7c183eceb9
Cybersasa <josephngunjiri2024@gmail.com>
2024-09-27 15:52:27 +0300

    ensembles of decision trees - random forests

--- decision_trees.py
+++ decision_trees.py
@@ -30,11 +30,12 @@ print('Accuracy of Decision tree (training) is {:.2f}'.format(100*cancer_tree.sc
 #pre-pruning the decision tree by adding a maximum depth
 
 X_train, X_test, y_train, y_test = train_test_split(load_breast_cancer().data, load_breast_cancer().target, stratify=load_breast_cancer().target, random_state=42)
-cancer_tree = DecisionTreeClassifier(random_state=0, max_depth=7).fit(X_train, y_train)
+cancer_tree = DecisionTreeClassifier(random_state=0, min_samples_leaf=15).fit(X_train, y_train)
 print('Accuracy of Decision tree is {:.2f}'.format(100*cancer_tree.score(X_test, y_test)))
 print('Accuracy of Decision tree (training) is {:.2f}'.format(100*cancer_tree.score(X_train, y_train)))
 
-#after adding max depth of 4, the accuracy of predictions increases 93.71% to 95.10%
+#after adding max depth (4), accuracy of predictions increases 93.71% to 95.10%
+#if using min_leaf_samples (15), accuracy of predictions increases to 95.80%
 from sklearn.tree import export_graphviz
 export_graphviz(cancer_tree, out_file="tree.dot", class_names=["malignant", "benign"], feature_names=load_breast_cancer().feature_names, impurity=False, filled=True)
 
@@ -43,6 +44,12 @@ import graphviz
 with open("tree.dot") as f:
     dot_graph = f.read()
 graphviz.Source(dot_graph)
+#displays an easy-to-understand visualization of the decision tree
+#print("Feature Importance:\n{}".format(cancer_tree.feature_importances_))
+#print('Feature vs Importance')
+
+#for feature, importance in zip(load_breast_cancer().feature_names, cancer_tree.feature_importances_):
+ #   print('{} - {:.2f}%'.format(feature, 100*importance))
 
 
 
